df5ba1a25ef11c44038218a2e9a2cd1ad582c9da
sam_arbid <samarbid13@users.noreply.github.com>
2019-07-22 11:24:18 +0200

    Updates

---
+++ precache-manifest.ca553b77e5d4f00ab55d3491d1e217de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2445c152065d4e1dfda30a15e93288b",
+    "url": "/pomodoro_clock_v001/index.html"
+  },
+  {
+    "revision": "c7db7666bdae2178f0cc",
+    "url": "/pomodoro_clock_v001/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pomodoro_clock_v001/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c7db7666bdae2178f0cc",
+    "url": "/pomodoro_clock_v001/static/js/main.ed4f4d00.chunk.js"
+  },
+  {
+    "revision": "1f9cd115eef6fec481c4",
+    "url": "/pomodoro_clock_v001/static/js/runtime~main.c65dc88b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro_clock_v001/static/media/logo.5d5d9eef.svg"
+  }
+]);
